b85a78dc82e5961a7ff2949ecfc52f7bdaeeb367
Jeeemi <144499548+Jeeemi@users.noreply.github.com>
2025-10-04 17:50:13 -0400

    help

--- rngName.py
+++ rngName.py
@@ -1,3 +1,34 @@
+
+#Graduation Training
+from google.colab import userdata
+from elevenlabs.client import ElevenLabs
+#import sounddevice as sd
+from scipy.io.wavfile import write
+import numpy as np
+
+
+client = ElevenLabs(
+    base_url="https://api.elevenlabs.io",
+    api_key = userdata.get('secretName')
+)
+
+#records audio
+#fs = 44100  # Sample rate
+#seconds = 4  # Duration of recording
+
+#myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+#sd.wait()  # Wait until recording is finished
+#write('audio.wav', fs, myrecording)
+
+with open("audio.wav", "rb") as audio_file:
+    transcript = client.speech_to_text.convert(
+        file = audio_file,
+        language_code = "eng",
+        model_id = "scribe_v1"   # Model for transcription
+    )
+    print(transcript.text)
+
+
 import csv
 import random
 
