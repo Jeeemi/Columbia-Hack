626e902a507d2a6747e250fa563cd5b7ae6b5156
Jeeemi <144499548+Jeeemi@users.noreply.github.com>
2025-10-04 19:09:15 -0400

    fixed rng name

--- rngName.py
+++ rngName.py
@@ -1,7 +1,7 @@
 import csv
 import random
 
-with open('/content/Both Names - Sheet1.csv', 'r', newline='') as csvfile:
+with open('Both Names - Sheet1.csv', 'r', newline='') as csvfile:
     csv_reader = csv.reader(csvfile)
     # Read all rows into a list
     rows = list(csv_reader)
