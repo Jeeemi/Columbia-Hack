d1077c16f1d549fb2635ada4db8e6342674c7c01
Jeeemi <144499548+Jeeemi@users.noreply.github.com>
2025-10-04 17:18:36 -0400

    Create rngName.py

---
+++ rngName.py
@@ -0,0 +1,10 @@
+import csv
+import random
+
+with open('/content/Both Names - Sheet1.csv', 'r', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    # Read all rows into a list
+    rows = list(csv_reader)
+    # Select a random row from the list
+    random_row = random.choice(rows)
+    print(','.join(random_row))
