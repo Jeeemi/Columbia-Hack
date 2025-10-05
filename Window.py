51a86915725e24366a805c16006fb88d68e446fb
yrret06 <tly03@nyit.edu>
2025-10-05 01:30:50 -0400

    Update Window.py

--- Window.py
+++ Window.py
@@ -1,8 +1,11 @@
 from tkinter import *
 from tkinter import ttk
 import SpeechToText
+import time
 
 stt = SpeechToText.SpeechToText()
+score = 0
+lives = 3
 name = stt.nameSelf
 height = 540
 width = 960
@@ -21,12 +24,39 @@ emotion = 0
 button = ttk.Button(frm, image = sprites[0])
 button.grid(column=int(width/2), row=int(height/2))
 
-def name(recorder):
+def name(recorder, but):
     recorder.record_audio()
     work = recorder.convert_speech_to_text()
+    work = work[:work.__len__() - 1]
+    right = recorder.correctPronounciation(work)
+    global stt
+    global score
+    global lives
+    print('Lives ' + str(lives))
+    if right:
+        print(right)
+        but.configure(image = sprite2)
+        but.photo = sprite2
+        time.sleep(1)
+        stt = SpeechToText.SpeechToText()
+        but.configure(image = sprite)
+        but.photo = sprite
+        score += 1
+    else:
+        print(right)
+        but.configure(image = sprite3)
+        but.photo = sprite3
+        if lives >= 0:
+            lives -= 1
+            name(recorder, but)
+        else:
+            stt = SpeechToText.SpeechToText()
+            score -= 1
+            lives = 3
     print(work)
+    print(right)
 
-record = ttk.Button(frm, text= 'record', command= lambda: name(stt)).grid(column= 0, row = 0)
+record = ttk.Button(frm, text= 'record', command= lambda: name(stt, button)).grid(column= 0, row = 0)
 
 root.mainloop()
 
